b252ddafd0f92289f6d39fdf423fd7ac1c04a357
xiaoye-hua <huag@kth.se>
2021-11-16 18:01:25 +0800

    minor change

--- src/Pipeline/XGBoostLRPipeline.py
+++ src/Pipeline/XGBoostLRPipeline.py
@@ -49,66 +49,6 @@ class XGBoostLRPipeline(XGBoostPipeline):
         self.pipeline = Pipeline(pipeline_lst)
         logging.info(f"Pipeline info: ")
         logging.info(self.pipeline)
-        # self.ordianl = transformer
-        # if train_params.get('pca_component_num', False):
-        #     pca_component_num = train_params['pca_component_num']
-        #     self.pca_component_num = pca_component_num
-        #     min_max = MinMaxScaler()
-        #     pca = PCA(n_components=pca_component_num)
-        #     X = min_max.fit_transform(X)
-        #     X = pca.fit_transform(X)
-        #     pipeline_lst.extend(
-        #         [
-        #             ('min_max', min_max)
-        #             , ('pca', pca)
-        #         ]
-        #     )
-        # self.xgb = XGBClassifier(**self.model_params)
-        # print(f"Model params are {self.xgb.get_params()}")
-
-        # if train_valid:
-        #     train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2)
-        #     train_params.update(
-        #         {
-        #             'verbose': True
-        #             , 'eval_metric': 'logloss'
-        #             , 'eval_set': [[train_X, train_y], [test_X, test_y]]
-        #         }
-        #     )
-
-
-
-
-        # if train_valid:
-        #     train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2)
-        #     # for trans in pipeline_lst:
-        #     #     eval_X = trans[1].transform(eval_X)
-        #     self.xgb.fit(X=X, y=y, verbose=True, eval_metric='logloss'
-        #                  , eval_set=[[train_X, train_y], [test_X, test_y]])
-        #     self._plot_eval_result()
-        #     train_params.update(
-        #         {
-        #             'verbose': True
-        #             , 'eval_metric': 'logloss'
-        #             , 'eval_set': [[train_X, train_y], [test_X, test_y]]
-        #         }
-        #     )
-        # else:
-        #     self.xgb.fit(X=X, y=y, verbose=True, eval_metric='logloss'
-        #                  , eval_set=[[X, y]])
-        # pipeline_lst.append(('model', self.xgb))
-        # self.pipeline = Pipeline(pipeline_lst)
-        # X = self.xgb.apply(X)#[:, 70:]
-        # logging.info(f"leave dim {X.shape}")
-        # self.one_hot = OneHotEncoder()
-        # logging.info(f"one hot...")
-        # X = self.one_hot.fit_transform(X)
-        # logging.info(f"finished one hot")
-        # self.lr = LogisticRegression(
-        # penalty='l1', solver='saga', verbose=1)
-        # logging.info(f"Logistic regression training...")
-        # self.lr.fit(X, y)
-        # logging.info(f"Logistic regression train finished")
 
     def predict_proba(self, X) -> pd.DataFrame:
         res = self.pipeline.predict_proba(X=X)[:, 1]
